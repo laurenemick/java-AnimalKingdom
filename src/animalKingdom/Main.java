526200a09fbfabe48284043cf89b6e89b13861e4
Lauren Emick <laurenemick6@gmail.com>
2020-08-05 12:19:51 -0700

    mvp complete

--- src/animalKingdom/Main.java
+++ src/animalKingdom/Main.java
@@ -63,20 +63,34 @@ public class Main {
 		animalList.sort((a1, a2) -> a2.yearNamed - a1.yearNamed); 
         animalList.forEach((a) -> System.out.println(a));
 
-
+        System.out.println();
         System.out.println("\n*** List all the animals alphabetically ***");
         animalList.sort((a1, a2) -> a1.name.compareToIgnoreCase(a2.name)); 
         animalList.forEach((a) -> System.out.println(a));
 
+        System.out.println();
         System.out.println("\n*** List all the animals order by how they move ***");
+        animalList.sort((a1, a2) -> a1.move().compareToIgnoreCase(a2.move())); 
+        animalList.forEach((a) -> System.out.println(a + "Move Value: " + a.move() + "\n"));
 
+        System.out.println();
         System.out.println("\n*** List only those animals the breath with lungs ***");
+        List<AbstractAnimals> filteredByLungs = filterAnimals(animalList, a -> a.breath() == "lungs");
+        filteredByLungs.forEach((a) -> System.out.println(a + "Breath value: " + a.breath() + "\n"));
 
+        System.out.println();
         System.out.println("\n*** List only those animals that breath with lungs and were named in 1758 ***");
+        List<AbstractAnimals> filteredByLungsAndYear = filterAnimals(filteredByLungs, a -> a.yearNamed == 1758);
+        filteredByLungsAndYear.forEach((a) -> System.out.println(a + "Breath value: " + a.breath() + "\n"));
 
+        System.out.println();
         System.out.println("\n*** List only those animals that lay eggs and breath with lungs ***");
+        List<AbstractAnimals> filteredByEggsAndLungs = filterAnimals(filteredByLungs, a -> a.reproduce() == "eggs");
+        filteredByEggsAndLungs.forEach((a) -> System.out.println(a + "Breath value: " + a.breath() + ", Reproduce Value: " + a.reproduce() + "\n"));
 
+        System.out.println();
         System.out.println("\n*** List alphabetically only those animals that were named in 1758 ***");
-
+        List<AbstractAnimals> filteredByYear = filterAnimals(animalList, a -> a.yearNamed == 1758);
+        filteredByYear.forEach((a) -> System.out.println(a));
 	}
 }
